f617ff5386b33314974c3ad42dcb73d4a0e16f97
Lamberto Martinez <lambpato@gmail.com>
2023-08-31 07:53:51 +0000

    created the new page directory

---
+++ todo-app/src/app/new/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <>
+      <header className='flex justify-between items-center mb-4'>
+        <h1 className='text-2x'>New</h1>
+      </header>
+      <form></form>
+    </>
+  );
+}
